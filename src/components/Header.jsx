4f4d7b0e47331534b90943087c616acad4065812
Erik Preininger <preininger.erik@mbhbank.hu>
2024-08-31 21:13:37 +0200

    WIP

---
+++ src/components/Header.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import Modal from 'react-modal';
+
+const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+    },
+  };
+
+function Header(){
+    let subtitle;
+    const [modalIsOpen, setIsOpen] = React.useState(false);
+    
+    function openModal() {
+        setIsOpen(true);
+    }
+
+    function afterOpenModal() {
+        // references are now sync'd and can be accessed.
+        subtitle.style.color = '#f00';
+    }
+
+    function closeModal() {
+        setIsOpen(false);
+    }
+
+    return(
+        <>
+            <h1>Header</h1>
+            <button onClick={openModal}>Open Modal</button>
+            <Modal
+                isOpen={modalIsOpen}
+                onAfterOpen={afterOpenModal}
+                onRequestClose={closeModal}
+                style={customStyles}
+                contentLabel="Example Modal"
+            >
+                <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2>
+                <button onClick={closeModal}>close</button>
+                <button>UniSat</button>
+                <button>Xverse</button>
+                <button>Magic Eden</button>
+                <form>
+                    <input />
+                </form>
+            </Modal>
+        </>
+    )
+}
+
+export default Header;
